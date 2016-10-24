9d9af41939cfce19f9032638e75bd7c8a8f23deb
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2016-10-24 15:46:50 -0400

    Add XK class
    This class handles most of the X-Keys devices. Devices with jog wheels,
    joysticks other special processing can override process_read function to
    add their own processing.

---
+++ src/xk.cpp
@@ -0,0 +1,115 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016 Dimitry Ishenko
+// Distributed under the GNU GPL v2. For full terms please visit:
+// http://www.gnu.org/licenses/gpl.html
+//
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+
+////////////////////////////////////////////////////////////////////////////////
+#include "xk.hpp"
+#include <climits> // CHAR_BIT
+
+using log::level;
+using namespace std::literals::chrono_literals;
+
+////////////////////////////////////////////////////////////////////////////////
+namespace pie
+{
+
+////////////////////////////////////////////////////////////////////////////////
+XK::XK(asio::io_service& io, const std::string& path, log::book clog) :
+    XK_base(io, path, std::move(clog)),
+    timer_(io)
+{
+    clog_(level::debug) << "Initializing" << std::endl;
+
+    auto store = read_desc();
+    auto desc = reinterpret_cast<pie::desc*>(store.data());
+
+    uid_ = desc->uid;
+    columns_ = desc->columns;
+    rows_ = desc->rows;
+    prev_.reset(new byte[columns_]);
+
+    clog_(level::debug) << "Detected device:"
+                        << " uid=" << uid_ << " columns=" << columns_ << " rows=" << rows_
+                        << std::endl;
+
+    ////////////////////
+    set_leds_on(leds::none);
+
+    set_level(light::blue, 128);
+    set_rows_on(light::blue, row::all);
+
+    set_level(light::red, 128);
+    set_rows_on(light::red, row::none);
+
+    ////////////////////
+    request_data();
+    schedule_read();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void XK::read()
+{
+    auto store = read_data();
+    if(store.size() + 2 < columns_) throw std::invalid_argument("Input short read");
+
+    auto pr = process_read(store);
+
+    for(auto index : std::get<0>(pr)) pressed_(index);
+    for(auto index : std::get<1>(pr)) released_(index);
+
+    schedule_read();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void XK::schedule_read()
+{
+    clog_(level::debug) << "Scheduling read" << std::endl;
+    timer_.expires_from_now(0s);
+    timer_.async_wait(std::bind(&XK::read, this));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+std::tuple<XK::press, XK::release> XK::process_read(const store& store)
+{
+    XK::press press;
+    XK::release release;
+
+    bool ps = store[1] & 0x01;
+    if(ps != ps_)
+    {
+        if(ps) press.insert(pie::prog);
+        else release.insert(pie::prog);
+        ps_ = ps;
+    }
+
+    ////////////////////
+    index_t index = 0;
+
+    auto ri = store.begin() + 2;
+    for(std::size_t c = 0; c < columns_; ++c, ++ri)
+    {
+        byte on  =  *ri & ~prev_[c];
+        byte off = ~*ri &  prev_[c];
+        prev_[c] =  *ri;
+
+        for(std::size_t r = 0; r < rows_; ++r)
+        {
+            if(on & 1) press.insert(index);
+            on >>= 1;
+
+            if(off & 1) release.insert(index);
+            off >>= 1;
+
+            ++index;
+        }
+        index += CHAR_BIT - rows_;
+    }
+
+    return std::make_tuple(press, release);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+}
---
+++ src/xk.hpp
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016 Dimitry Ishenko
+// Distributed under the GNU GPL v2. For full terms please visit:
+// http://www.gnu.org/licenses/gpl.html
+//
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef PIE_XK_HPP
+#define PIE_XK_HPP
+
+#include "sig/signal.hpp"
+#include "xk_base.hpp"
+
+#include <asio/system_timer.hpp>
+#include <memory>
+#include <set>
+#include <tuple>
+
+////////////////////////////////////////////////////////////////////////////////
+namespace pie
+{
+
+using index_t = int;
+static constexpr index_t prog = -1;
+
+////////////////////////////////////////////////////////////////////////////////
+class XK : public XK_base
+{
+public:
+    ////////////////////
+    XK(asio::io_service& io, const std::string& path, log::book = log::book());
+
+    ////////////////////
+    auto uid() const noexcept { return uid_; }
+    auto columns() const noexcept { return columns_; }
+    auto rows() const noexcept { return rows_; }
+
+    ////////////////////
+    sig::signal_proxy<void(int)>& pressed() { return pressed_; }
+    sig::signal_proxy<void(int)>& released() { return released_; }
+
+protected:
+    ////////////////////
+    asio::system_timer timer_;
+
+    int uid_ = 0;
+    std::size_t columns_ = 0, rows_ = 0;
+
+    bool ps_ = false;
+    std::unique_ptr<byte[]> prev_;
+
+    void read();
+    void schedule_read();
+
+    using press = std::set<index_t>;
+    using release = std::set<index_t>;
+    virtual std::tuple<press, release> process_read(const store&);
+
+    ////////////////////
+    sig::signal<void(int)> pressed_;
+    sig::signal<void(int)> released_;
+};
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+#endif // XK_HPP
